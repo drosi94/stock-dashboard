ff0e6f2d4f3a7b170f6a2b9ca1df6073cc342447
Vasileios Drosatos <b.drosat@hotmail.com>
2020-11-07 16:51:33 +0200

    Add Header component

---
+++ src/View/Layout/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './styles.module.css';
+
+function Header() {
+  return (
+    <header className={classes.header}>
+      <h1>Stock Dashboard</h1>
+    </header>
+  );
+}
+
+export default Header;
