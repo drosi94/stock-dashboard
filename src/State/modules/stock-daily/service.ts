f768494c2e7bdef05c7aa8ca7b4f21fff4161105
Vasileios Drosatos <b.drosat@hotmail.com>
2020-11-07 16:50:13 +0200

    Fix mapping of the data

--- src/State/modules/stock-daily/service.ts
+++ src/State/modules/stock-daily/service.ts
@@ -1,6 +1,21 @@
 import API from '../../utils/stock-api';
 import { DailyStockResponse } from './types';
 
+const handleResults = (data: any): DailyStockResponse[] => {
+  const keys = Object.keys(data);
+  return keys.map(
+    (key) =>
+      ({
+        date: new Date(key),
+        open: parseFloat(data[key]['1. open']),
+        high: parseFloat(data[key]['2. high']),
+        low: parseFloat(data[key]['3. low']),
+        close: parseFloat(data[key]['4. close']),
+        volume: parseFloat(data[key]['5. volume']),
+      } as DailyStockResponse)
+  );
+};
+
 /**
  * Fetches the daily stocks for the selected symbol
  * @param {string} symbol, The symbol of the stock
@@ -9,10 +24,14 @@ export const fetchTimeSeriesDaily = async (
   symbol: string
 ): Promise<DailyStockResponse[]> => {
   const apiResponse = await API.get('/query2', {
-    params: { function: 'TIME_SERIES_DAILY', symbol },
+    params: {
+      function: 'TIME_SERIES_DAILY',
+      symbol,
+      outputsize: 'compact',
+    },
   });
   if (apiResponse.data['Time Series (Daily)']) {
-    return apiResponse.data as DailyStockResponse[];
+    return handleResults(apiResponse.data['Time Series (Daily)']);
   } else {
     return Promise.reject({
       message: 'No data found',
